e9198b07da49773a217d5c511f64c803bd97a151
ShraddhaK1651 <shraddhakolli334@gmail.com>
2026-02-23 18:07:14 +0530

    Create COMBINATIONSUM4.java

---
+++ DP/COMBINATIONSUM4.java
@@ -0,0 +1,32 @@
+Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+The test cases are generated so that the answer can fit in a 32-bit integer.
+Example 1:
+Input: nums = [1,2,3], target = 4
+Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.
+
+  class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int j = 1; j <= target; j++) {
+            for (int num : nums) {
+                if (num <= j) {
+                    dp[j] += dp[j - num];
+                }
+            }
+        }
+
+        return dp[target];
+    }
+}
