916c6efccbe79f5b1ecbb266747b938cb6efae29
ShraddhaK1651 <shraddhakolli334@gmail.com>
2026-03-01 10:11:56 +0530

    Update 0-1 KNAPSACK.java

--- DP/0-1 KNAPSACK.java
+++ DP/0-1 KNAPSACK.java
@@ -33,3 +33,54 @@ public class Classroom {
         System.out.println(knapsack(val, wt, W, val.length));
     }
 }
+
+
+
+MEMOIZATION CODE
+
+
+import java.util.Arrays;
+
+public class KnapsackMemoization {
+
+    static int[][] dp;
+
+    static int knapsack(int[] wt, int[] val, int W, int n) {
+
+        // Base Condition
+        if (n == 0 || W == 0)
+            return 0;
+
+        // If already calculated
+        if (dp[n][W] != -1)
+            return dp[n][W];
+
+        // Choice Diagram
+        if (wt[n - 1] <= W) {
+            dp[n][W] = Math.max(
+                    val[n - 1] + knapsack(wt, val, W - wt[n - 1], n - 1),  // include
+                    knapsack(wt, val, W, n - 1)                            // exclude
+            );
+        } else {
+            dp[n][W] = knapsack(wt, val, W, n - 1);
+        }
+
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+
+        int[] wt = {1, 3, 4, 5};
+        int[] val = {1, 4, 5, 7};
+        int W = 7;
+        int n = wt.length;
+
+        dp = new int[n + 1][W + 1];
+
+        // Initialize with -1
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        System.out.println("Maximum Profit = " + knapsack(wt, val, W, n));
+    }
+}
