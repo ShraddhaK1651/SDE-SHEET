f24ed0946ed1d3ce1b584c589713cd7f875e83fa
ShraddhaK1651 <shraddhakolli334@gmail.com>
2026-03-05 22:19:40 +0530

    Update and rename MCM_TABULATION to MCM_TABULATION.java

---
+++ DP/MCM_TABULATION.java
@@ -0,0 +1,88 @@
+public class MCMTabulation {
+
+    public static int mcmTab(int arr[]) {
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+
+        // initialization
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 0;
+        }
+
+        // bottom-up approach
+        for (int len = 2; len <= n - 1; len++) {
+            for (int i = 1; i <= n - len; i++) {
+                int j = i + len - 1;   // column
+                dp[i][j] = Integer.MAX_VALUE;
+
+                for (int k = i; k <= j - 1; k++) {
+
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k + 1][j];
+                    int cost3 = arr[i - 1] * arr[k] * arr[j];
+
+                    dp[i][j] = Math.min(dp[i][j], cost1 + cost2 + cost3);
+                }
+            }
+        }
+
+        return dp[1][n - 1];
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 3, 4, 3};
+        System.out.println("Minimum multiplication cost: " + mcmTab(arr));
+    }
+}
+
+
+MEMOIZATION CODE
+import java.util.Arrays;
+
+public class MCMMemoization {
+
+    static int[][] dp;
+
+    public static int mcmMemo(int arr[], int i, int j) {
+
+        // base case
+        if (i == j) {
+            return 0;
+        }
+
+        // if already calculated
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int minCost = Integer.MAX_VALUE;
+
+        for (int k = i; k <= j - 1; k++) {
+
+            int cost1 = mcmMemo(arr, i, k);
+            int cost2 = mcmMemo(arr, k + 1, j);
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+
+            int totalCost = cost1 + cost2 + cost3;
+
+            minCost = Math.min(minCost, totalCost);
+        }
+
+        return dp[i][j] = minCost;
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = {1, 2, 3, 4, 3};
+        int n = arr.length;
+
+        dp = new int[n][n];
+
+        // initialize dp with -1
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        System.out.println("Minimum multiplication cost: " + mcmMemo(arr, 1, n - 1));
+    }
+}
